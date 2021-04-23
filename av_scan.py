68af1b81effc52da62c9606de49f6dbbb996e4af
0xD3caf <66028416+0xD3caf@users.noreply.github.com>
2021-04-23 11:21:43 -0500

    Update av_scan.py

--- av_scan.py
+++ av_scan.py
@@ -1,59 +1,61 @@
 #Anitvuris / Malware Test
 #Mackenzie Summers
  
+#Connects via SSH to target box and locates files
+#then scans files and checks for malware
+#Run from Win10
+ 
 import requests, hashlib, time, os, paramiko
 from time import sleep
  
 def scan(dir):
-	client = paramiko.SSHClient()															#CHECK STARTS HERE
+	hits = 0
+	total_checked = 0
+	client = paramiko.SSHClient()													
 	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	client.connect("10.12.0.30", username="root", password="password")
-	os.mkdir("client_files")
-	sftp = ssh.open_sftp()
+	sftp = client.open_sftp()
 	sftp.chdir(dir)
-	for num in range(51):
-		if num >= 9:
-			curr_malware = "Malware0" + num
-		else:
-			curr_malware = "Malware" + num
-		sftp.get("/root/files/" + curr_malware, "C:Users\\student\\Desktop\\scripts")		#CHECK
- 	sftp.close()
-	client.close()
-	API_KEY = "ad339e729d83f12a897462dd0ccbd9e193b5e7cb1e12b8045a6cc877afa5d7b3"
-	url = 'https://www.virustotal.com/vtapi/v2/file/scan'
-	os.chdir("scripts")
-	file_list = os.listdir()																#CHECK STOPS HERE
+	file_list = sftp.listdir()
+	API_KEY = "VALUE"
+	scan_url = 'https://www.virustotal.com/vtapi/v2/file/scan'
+	report_url = 'https://www.virustotal.com/vtapi/v2/file/report'
 	for file in file_list:
-		files = ({'file': (file, open(file,'rb'))})
+		f = sftp.file(file, mode='r')
+		files = {'file': (file ,f)}
 		try:
-			response = requests.post(url, files=files, params={'apikey': API_KEY})
+			response = requests.post(scan_url, files=files, params={'apikey': API_KEY})
 			response_data = response.json()
 			resource = response_data.get("resource")
 		except:
 			print("looks like this file didnt work")
-		url = 'https://www.virustotal.com/vtapi/v2/file/report'
 		for i in range(6):	
 			try:
-				resp = requests.get(url, params={'apikey': API_KEY, 'resource': resource})
-				print(resp.json())
+				resp = requests.get(report_url, params={'apikey': API_KEY, 'resource': resource})
 				resp = resp.json()
+				total_checked = resp.get("total") 
+				hits = resp.get("positives")
 				break
 			except:
 				sleep(5)
 				continue
-		total_checked = resp.get("total") 
-		hits = resp.get("positives")
+		f.close()
 		if hits == 0:
 			percent = 0
 		else:
 			percent = total_checked/hits
 		if percent >= .05:
 			print("MALWARE FOUND")
-			print("File: {} is Malware".format(file))
-			break
+			print("File: {} is Malware".format(str(file)))
+			sftp.close()		#
+			client.close()		#
+			break				#remove for full scan even after malware found
 		else:
-			sleep(12)
- 
+			sleep(15)
+	if sftp:
+		sftp.close()
+	if client:
+		client.close()
 print("\nAntiVirus Check\n")
 print("This may take some time")
 scan("/root/files/")
